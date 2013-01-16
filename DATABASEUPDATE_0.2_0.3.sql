daa8058cc51d55c423c83e509df72d9c6e89e5c7
Benjamin 'blindCoder' Schieder <blindcoder@scavenger.homeip.net>
2013-01-16 13:05:27 +0100

    re-remove documentation that I forgot I moved into doc/ earlier

--- DATABASEUPDATE_0.2_0.3.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `entries` ADD KEY `isread` (`isread`);
-ALTER TABLE `entries` ADD KEY `feedID` (`feedID`);
-ALTER TABLE `feeds` CHANGE `url` `url` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE `feeds` ADD `parentID` BIGINT DEFAULT '0' NOT NULL ;
-ALTER TABLE `feeds` DROP INDEX `url`;
-ALTER TABLE `feeds` ADD INDEX ( `url` );
-ALTER TABLE `entries` DROP INDEX `link` ,
-      ADD UNIQUE `link` ( `link` ( 512 ) , `feedID` ) 
--- DATABASEUPDATE_0.6_0.7.sql
+++
@@ -1,3 +0,0 @@
-SELECT endID INTO @a FROM feeds WHERE startID = 1;
-UPDATE `feeds` SET endID = endID + 2 WHERE startID = 1;
-INSERT INTO `feeds` (startID, endID, name, url) VALUES (@a, @a+1, "Searchresults", "SEARCHRESULTS");
