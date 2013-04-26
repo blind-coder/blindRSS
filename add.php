10f6e61423fc2e5ec44f5cd2526395d5196046fc
Benjamin 'blindCoder' Schieder <blindcoder@scavenger.homeip.net>
2013-04-26 09:50:40 +0200

    convert output of add.php to json; check if parent is still a directory

--- add.php
+++ add.php
@@ -1,10 +1,23 @@
 <?
 include("etc/config.php");
-header ("Content-Type: text/xml");
-echo "<?xml version=\"1.0\"?>\n\n";
+header ("Content-Type: text/plain");
+
+function my_mysql_query($SQL){
+	$q = mysql_query($SQL);
+	if (mysql_error()){
+		$data["status"] = "error";
+		$data["msg"] = mysql_error();
+		echo json_encode($data);
+		exit();
+	}
+	return $q;
+}
+
+$data["status"] = "error";
+$data["msg"] = "Unknown error!";
 
 if ($ISDEMO){
-	echo "<status>This is just a demo page.</status>";
+	$data["msg"] = "This is just a demo page!";
 } else {
 	mysql_connect($MYSQL_HOST, $MYSQL_USER, $MYSQL_PASS);
 	mysql_select_db($MYSQL_DB);
@@ -19,29 +32,32 @@ if ($ISDEMO){
 		if ($_GET["name"] == "")
 			$_GET["name"] = "Unknown feed";
 	}
+
 	foreach (Array("name", "url", "parentID") as $x)
 		$_GET[$x] = mysql_real_escape_string($_GET[$x]);
-	$q = mysql_query("SELECT * FROM feeds WHERE `url` = '".$_GET['url']."' AND `url` != ''");
+
+	$q = my_mysql_query("SELECT * FROM feeds WHERE `url` = '".$_GET['url']."' AND `url` != ''");
 	if (mysql_num_rows($q) > 0){
 		$r = mysql_fetch_object($q);
-		echo "<status>Duplicate URL. URL in use in feed $r->name!</status>";
+		$data["msg"] = "Duplicate URL. URL in use in feed {$r->name}!";
 	} else {
-		$q = mysql_query("SELECT * FROM feeds WHERE ID = '".$_GET['parentID']."'");
+		$q = my_mysql_query("SELECT * FROM feeds WHERE ID = '".$_GET['parentID']."'");
 		$r = mysql_fetch_object ($q);
-		mysql_query("UPDATE feeds SET endID=endID+2 WHERE endID >= $r->endID");
-		mysql_query("UPDATE feeds SET startID=startID+2 WHERE startID >= $r->endID"); # $r->endID is correct!
-		$q = mysql_query("INSERT INTO feeds (startID, endID, name, url) VALUES (".
-				 "".$r->endID.", ".
-				 "".($r->endID+1).", ".
-				 "'".$_GET['name']."', ".
-				 "'".$_GET['url']."')");
-		echo "<status>";
-		if (mysql_error()){
-			echo mysql_error();
+		if ($r->url != ""){
+			$data["msg"] = "Feed {$r->ID} with name {$r->name} is NOT a directory!";
 		} else {
-			echo "Feed added";
+			mysql_query("UPDATE feeds SET endID=endID+2 WHERE endID >= $r->endID");
+			mysql_query("UPDATE feeds SET startID=startID+2 WHERE startID >= $r->endID"); # $r->endID is correct!
+			$q = mysql_query("INSERT INTO feeds (startID, endID, name, url) VALUES (".
+					 "".$r->endID.", ".
+					 "".($r->endID+1).", ".
+					 "'".$_GET['name']."', ".
+					 "'".$_GET['url']."')");
+			$data["status"] = "OK";
+			$data["msg"] = "Feed {$_GET['name']} added!";
 		}
-		echo "</status>";
 	}
 }
+
+echo json_encode($data);
 ?>
